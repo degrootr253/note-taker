4ddd0934e8f4e17076e16f5e21318a2dcae007df
Ryan DeGroot <degrootr253@gmail.com>
2021-10-26 20:51:55 -0700

    Adding changes to server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const { notStrictEqual } = require('assert');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    db.push(newNote);
+
+    fs.writeFile(__dirname, './db/db.jon', JSON.stringify(db), function (err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log('Note Saved!')
+        }
+    });
+    return res.json(newNote);
+});
+
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
